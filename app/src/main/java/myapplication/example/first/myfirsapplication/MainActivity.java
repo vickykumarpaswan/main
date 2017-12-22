2b544166681cf942a0ce39d4220506de3dd112d0
vickykumarpaswan <34579303+vickykumarpaswan@users.noreply.github.com>
2017-12-22 12:02:40 -0500

    Initial commit

---
+++ app/src/main/java/myapplication/example/first/myfirsapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package myapplication.example.first.myfirsapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+private MyAnswers mMyAnswers= new  MyAnswers();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        private void TestUpload (){}
+        //Declare View variables assign them the views from the layout file
+
+        TextView answerLabel = findViewById(R.id.)
+        Button getAnswerButton;
+
+    }
+}
---
+++ app/src/main/java/myapplication/example/first/myfirsapplication/MyAnswers.java
@@ -0,0 +1,8 @@
+package myapplication.example.first.myfirsapplication;
+
+/**
+ * Created by DELL on 12/18/2017.
+ */
+
+public class MyAnswers {
+}
